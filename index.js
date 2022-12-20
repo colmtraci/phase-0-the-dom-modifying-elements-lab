e8af6a104dddac1a0b02a538dfacdf6e31cb2b7d
Colm Traci <ctraci@me.com>
2022-12-19 20:29:56 -0800

    commit tree 85c9f298765eb57101a2833c222966fdeb93e21e

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+main.remove(); // Remove Elements from the DOM
+
+const newHeader = document.createElement('h1'); //Add Elements to the DOM
+
+newHeader.id = 'victory';
+
+newHeader.innerHTML = "Colm is the champion"; //Add Elements to the DOM via innerHTML
